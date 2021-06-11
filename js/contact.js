3c7ed784d6f9499f78daa06f5d3853af2a097b3e
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-11 11:46:16 +0530

    full address book

---
+++ js/contact.js
@@ -0,0 +1,169 @@
+let isUpdate = false;
+let addressBookObj = {};
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".error-text-output");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      checkName(name.value);
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
+
+  const mobileNo = document.querySelector("#mobile");
+  const mobileError = document.querySelector(".error-mobile-output");
+  mobileNo.addEventListener("input", function () {
+    if (mobileNo.value.length == 0) {
+      mobileError.textContent = "";
+      return;
+    }
+    try {
+      checkPhone(mobileNo.value);
+      mobileError.textContent = "";
+    } catch (e) {
+      mobileError.textContent = e;
+    }
+  });
+
+  const zip = document.querySelector("#zip");
+  const zipErr = document.querySelector(".error-zip-output");
+  zip.addEventListener("input", function () {
+    if (zip.value.length == 0) {
+      zipErr.textContent = "";
+      return;
+    }
+    try {
+      checkZip(zip.value);
+      zipErr.textContent = "";
+    } catch (e) {
+      zipErr.textContent = e;
+    }
+  });
+  checkForUpdate();
+});
+
+const save = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  try {
+    setAddressBookObject();
+    if (site_properties.use_local_storage.match("true")) {
+      createAndupdateStorage();
+      resetForm();
+      window.location.replace(site_properties.home_page);
+    } else {
+      createOrUpdateAddressBook();
+    }
+  } catch (e) {
+    return;
+  }
+  //alert(addressBookObj.toString());
+};
+
+const createOrUpdateAddressBook = () => {
+  let postURL = site_properties.server_url;
+  let methodCall = "POST";
+  if (isUpdate) {
+    methodCall = "PUT";
+    postURL = postURL + addressBookObj.id.toString();
+  }
+  makeServiceCall(methodCall, postURL, true, addressBookObj)
+    .then((responseText) => {
+      resetForm();
+      window.location.replace(site_properties.home_page);
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+const setAddressBookObject = () => {
+  if (!isUpdate && site_properties.use_local_storage.match("true")) {
+    addressBookObj.id = createNewConatctId();
+  }
+  addressBookObj._name = document.querySelector("#name").value;
+  addressBookObj._phoneNo = document.querySelector("#mobile").value;
+  addressBookObj._address = document.querySelector("#address").value;
+  addressBookObj._city = document.querySelector("#city").value;
+  addressBookObj._state = document.querySelector("#state").value;
+  addressBookObj._zipCode = document.querySelector("#zip").value;
+};
+
+const createNewConatctId = () => {
+  let personID = localStorage.getItem("ContactID");
+  personID = !personID ? 1 : (parseInt(personID) + 1).toString();
+  localStorage.setItem("ContactID", personID);
+  return personID;
+};
+
+const createAndupdateStorage = () => {
+  let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+  if (addressBookList) {
+    let addressData = addressBookList.find(
+      (address) => address.id == addressBookObj.id
+    );
+    if (!addressData) {
+      addressBookList.push(addressBookObj);
+    } else {
+      const index = addressBookList
+        .map((address) => address.id)
+        .indexOf(addressData.id);
+      addressBookList.splice(index, 1, addressBookObj);
+    }
+  } else {
+    addressBookList = [addressBookObj];
+  }
+  // alert("Local Contact Details: "+addressBookObj.toString());
+  localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+};
+
+const checkForUpdate = () => {
+  const addressBookJson = localStorage.getItem("editContact");
+  isUpdate = addressBookJson ? true : false;
+  if (!isUpdate) return;
+  addressBookObj = JSON.parse(addressBookJson);
+  setForm();
+};
+
+const setForm = () => {
+  setValue("#name", addressBookObj._name);
+  setValue("#mobile", addressBookObj._phoneNo);
+  setValue("#address", addressBookObj._address);
+  document.querySelector("#city").value = addressBookObj._city;
+  document.querySelector("#state").value = addressBookObj._state;
+  document.querySelector("#zip").value = addressBookObj._zipCode;
+};
+
+function resetForm() {
+  document.querySelector("#name").value = "";
+  document.querySelector("#mobile").value = "";
+  document.querySelector("#address").value = "";
+  document.querySelector("#city").value = "";
+  document.querySelector("#state").value = "";
+  document.querySelector("#zip").value = "";
+}
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+};
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+};
+
+const setSelectedValue = (propertyValue, value) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach((item) => {
+    if (value.includes(item.values)) {
+      item.checked = true;
+    } else if (item.value === value) item.checked = true;
+  });
+};
---
+++ js/home.js
@@ -0,0 +1,93 @@
+let addressBookList;
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (site_properties.use_local_storage.match("true")) {
+    getAddressBookDataFromStorage();
+  } else getAddressBookDataFromServer();
+});
+
+const getAddressBookDataFromStorage = () => {
+  addressBookList = localStorage.getItem("AddressBookList")
+    ? JSON.parse(localStorage.getItem("AddressBookList"))
+    : [];
+  processAddressBookDataResponse();
+};
+
+const processAddressBookDataResponse = () => {
+  createInnerHtml();
+  localStorage.removeItem("editContact");
+};
+
+const getAddressBookDataFromServer = () => {
+  makeServiceCall("GET", site_properties.server_url, true)
+    .then((responseText) => {
+      addressBookList = JSON.parse(responseText);
+      processAddressBookDataResponse();
+    })
+    .catch((error) => {
+      console.log("GET Error Status: " + JSON.stringify(error));
+      addressBookList = [];
+      processAddressBookDataResponse();
+    });
+};
+
+const createInnerHtml = () => {
+  const headerHtml =
+    "<th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
+    "<th>ZipCode</th><th>PhoneNo</th><th></th>";
+  let innerHtml = `${headerHtml}`;
+  if (addressBookList.length == 0) return;
+  for (const addressBookData of addressBookList) {
+    innerHtml = `${innerHtml}
+     <tr>
+     <td>${addressBookData._name}</td>
+     <td>${addressBookData._address}</td>
+     <td>${addressBookData._city}</td>
+     <td>${addressBookData._state}</td>
+     <td>${addressBookData._zipCode}</td>
+     <td>${addressBookData._phoneNo}</td>
+     <td>
+     <img id="${addressBookData.id}" onclick="remove(this)" alt="delete"
+     src="../Assets/delete-black-18dp.svg" width = "25">
+     <img id="${addressBookData.id}" alt="edit" onclick="update(this)"
+     src="../Assets/outline_edit_black_24dp.png">
+     </td>
+     </tr>
+    `;
+  }
+  document.querySelector("#table-display").innerHTML = innerHtml;
+};
+
+const remove = (node) => {
+  let addressBookData = addressBookList.find(
+    (addData) => addData.id == node.id
+  );
+  if (!addressBookData) return;
+  const index = addressBookList
+    .map((addData) => addData.id)
+    .indexOf(addressBookData.id);
+  addressBookList.splice(index, 1);
+  if (site_properties.use_local_storage.match("true")) {
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+    createInnerHtml();
+    window.location.replace(site_properties.home_page);
+  } else {
+    const deleteURl =
+      site_properties.server_url + addressBookData.id.toString();
+    makeServiceCall("DELETE", deleteURl, false)
+      .then((responseText) => {
+        createInnerHtml();
+      })
+      .catch((error) => {
+        console.log("DELETE Error Status: " + JSON.stringify(error));
+      });
+  }
+};
+
+const update = (node) => {
+  let addressBookData = addressBookList.find(
+    (addData) => addData.id == node.id
+  );
+  if (!addressBookData) return;
+  localStorage.setItem("editContact", JSON.stringify(addressBookData));
+  window.location.replace(site_properties.AddressBook_page);
+};
